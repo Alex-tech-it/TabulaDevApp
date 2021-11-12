ddb8095a7a0c0a6b81028c658e74a9fe2988f8ff
Alex-tech-it <58144587+Alex-tech-it@users.noreply.github.com>
2021-11-12 19:12:45 +0300

    Beta Drag & Drop

---
+++ TabulaDevApp/MVVM/Models/DragAndDropUIList.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace TabulaDevApp.MVVM.Models
+{
+    public class DragAndDropUIList
+    {
+        public StackPanel DragAndDropPanel { get; set; }
+        public  ObservableCollection<Border> DragAndDropCardList { get; set; }
+
+        public DragAndDropUIList()
+        {
+            DragAndDropPanel = new StackPanel();
+            DragAndDropCardList = new ObservableCollection<Border>();
+        }
+    }
+}
--- TabulaDevApp/MVVM/ViewModels/KanbanBoardViewModel.cs
+++ TabulaDevApp/MVVM/ViewModels/KanbanBoardViewModel.cs
@@ -15,10 +15,14 @@ namespace TabulaDevApp.MVVM.ViewModels
 {
     class KanbanBoardViewModel : ObservableObject
     {
-        private NavigationStore _navigationStore;   // Navigation to add New Card
-        private StackPanel _currentStackPanel;      // Shows tabel
-        private KanbanBoardModel _kanbanBoardModel; // Model contains structure of tabel
+        private NavigationStore _navigationStore;           // Navigation to add New Card
+        private StackPanel _currentStackPanel;              // Shows tabel
+        private KanbanBoardModel _kanbanBoardModel;         // Model contains structure of tabel
+        private Card _dragAndDropCurrentCard;
+        private int _dragAndDropStoreCardIndex;
+        private int _dragAndDropStoreColumnIndex;
 
+        // Gettter & Setter
         public StackPanel CurrentStackPanel
         {
             get => _currentStackPanel;
@@ -37,15 +41,20 @@ namespace TabulaDevApp.MVVM.ViewModels
                 OnPropertyChanged();
             }
         }
-        
+
+        // Ctors
         public KanbanBoardViewModel()
         {
             
         }
         public KanbanBoardViewModel(NavigationStore navigation, KanbanBoardModel model)
         {
+            _dragAndDropStoreCardIndex = -1;
+            _dragAndDropStoreColumnIndex = -1;
+
             kanbanBoardModel = model;
             _navigationStore = navigation;
+
             StartUpGrid();
             UpdateStackPanel();
         }
@@ -96,7 +105,8 @@ namespace TabulaDevApp.MVVM.ViewModels
             Border newColumn = new Border();
             TextBox TitleColumn = new TextBox();
             Button ButtonNewCard = new Button();
-            StackPanel stackPanel = new StackPanel();
+            Canvas stackPanel = new Canvas();
+
 
             // Style Column
             newColumn.BorderThickness = new Thickness(0);
@@ -104,6 +114,7 @@ namespace TabulaDevApp.MVVM.ViewModels
             newColumn.CornerRadius = new CornerRadius(10);
             newColumn.Margin = new Thickness(0, 0, 10, 0);
             newColumn.VerticalAlignment = VerticalAlignment.Top;
+            newColumn.AllowDrop = true;
 
             // Style Title 
             TitleColumn.Background = Brushes.Transparent;
@@ -142,16 +153,72 @@ namespace TabulaDevApp.MVVM.ViewModels
             ButtonNewCard.Click += CreateNewCard;
             ButtonNewCard.Content = "Добавить карточку";
 
+            stackPanel.Width = 225;
+            stackPanel.Height = 0;
+            stackPanel.Name = "Column_" + Convert.ToString(indexColumn); ;
             stackPanel.Margin = new Thickness(5, 5, 5, 5);
             stackPanel.HorizontalAlignment = HorizontalAlignment.Center;
+
+            Canvas.SetLeft(TitleColumn, 0);
+            Canvas.SetTop(TitleColumn, 0);
             stackPanel.Children.Add(TitleColumn);
 
+            stackPanel.Drop += Column_Drop;
+            stackPanel.DragOver += Column_DragOver;
+            stackPanel.DragLeave += Column_DragLeave;
+            //stackPanel.DragEnter += Column_DragEnter;
+
+            double countHeight = 50;
             for (int i = 0; i < kanbanBoardModel.Lists[indexColumn].Cards.Count; i++)
-            {
+            { 
+                if(_dragAndDropStoreCardIndex != -1 && _dragAndDropStoreColumnIndex != -1)
+                {
+                    if (_dragAndDropStoreCardIndex >= kanbanBoardModel.Lists[indexColumn].Cards.Count &&
+                        _dragAndDropStoreColumnIndex == indexColumn &&
+                        i == kanbanBoardModel.Lists[indexColumn].Cards.Count - 1)
+                    {
+                        kanbanBoardModel.Lists[indexColumn].Cards[i].id = Convert.ToString(i) + "_" + indexColumn;
+                        Border newInCard = CreateUINewCard(kanbanBoardModel.Lists[indexColumn].Cards[i]);
+
+                        Canvas.SetLeft(newInCard, 5);
+                        Canvas.SetTop(newInCard, countHeight);
+                        stackPanel.Children.Add(newInCard);
+                        countHeight += 115;
+
+                        Border emptyCard = CreateUIEmptyCard();
+                        Canvas.SetLeft(emptyCard, 5);
+                        Canvas.SetTop(emptyCard, countHeight);
+                        stackPanel.Children.Add(emptyCard);
+                        countHeight += 115;
+                        _dragAndDropStoreCardIndex = -1;
+                        _dragAndDropStoreColumnIndex = -1;
+                        continue;
+                    }
+                    else if (_dragAndDropStoreCardIndex == i && _dragAndDropStoreColumnIndex == indexColumn)
+                    {
+                        Border emptyCard = CreateUIEmptyCard();
+                        Canvas.SetLeft(emptyCard, 5);
+                        Canvas.SetTop(emptyCard, countHeight);
+                        stackPanel.Children.Add(emptyCard);
+                        countHeight += 115;
+                        _dragAndDropStoreCardIndex = -1;
+                        _dragAndDropStoreColumnIndex = -1;
+                    }
+                    
+                }
+
                 kanbanBoardModel.Lists[indexColumn].Cards[i].id = Convert.ToString(i) + "_" + indexColumn;
-                stackPanel.Children.Add(CreateUINewCard(kanbanBoardModel.Lists[indexColumn].Cards[i]));
+                Border newCard = CreateUINewCard(kanbanBoardModel.Lists[indexColumn].Cards[i]);
+
+                Canvas.SetLeft(newCard, 5);
+                Canvas.SetTop(newCard, countHeight);
+                stackPanel.Children.Add(newCard);
+                countHeight += 115;
             }
 
+            stackPanel.Height = (countHeight > 80) ? countHeight + 30 : 80;
+            Canvas.SetLeft(ButtonNewCard, 0);
+            Canvas.SetTop(ButtonNewCard, stackPanel.Height - 30);
             stackPanel.Children.Add(ButtonNewCard);
             newColumn.Child = stackPanel;
 
@@ -169,14 +236,21 @@ namespace TabulaDevApp.MVVM.ViewModels
 
             UpdateStackPanel();
         }
-        
+
+        // Creat UI Card
         private Border CreateUINewCard(Card card)
         {
             SolidColorBrush foreground = new SolidColorBrush { Color = Color.FromRgb(244, 245, 245) };
             SolidColorBrush background = new SolidColorBrush { Color = Color.FromRgb(47, 48, 55) };
+            SolidColorBrush backgroundTitle = new SolidColorBrush { Color = Color.FromRgb(28, 28, 33) };
 
             Border newCard = new Border();
+            Border titleCard = new Border();
+            Border rectUnder = new Border();
+
             StackPanel newPanel = new StackPanel();
+            Grid titleGrid = new Grid();
+
             TextBlock textBlockTitle = new TextBlock();
             TextBlock textDescription = new TextBlock();
 
@@ -186,12 +260,26 @@ namespace TabulaDevApp.MVVM.ViewModels
             newCard.CornerRadius = new CornerRadius(5);
             newCard.Margin = new Thickness(0, 0, 0, 5);
             newCard.Width = 215;
+            newCard.Height = 105;
             newCard.Name = "Card_" + card.id;
-            newCard.MouseDown += OnClickCard;
+            //newCard.MouseDown += OnClickCard;
+            //newCard.MouseLeftButtonDown += OnClickCard;
+            newCard.MouseMove += Card_MouseMove;
+
+            rectUnder.Background = backgroundTitle;
+            rectUnder.VerticalAlignment = VerticalAlignment.Bottom;
+            rectUnder.Width = 215;
+            rectUnder.Height = 5;
+
+            titleCard.Background = backgroundTitle;
+            titleCard.CornerRadius = new CornerRadius(5);
+            titleCard.VerticalAlignment = VerticalAlignment.Top;
+            titleCard.Width = 215;
+            titleCard.Height = 20;
 
             textBlockTitle.FontSize = 14;
             textBlockTitle.TextWrapping = TextWrapping.Wrap;
-            textBlockTitle.MaxHeight = 40;
+            textBlockTitle.MaxHeight = 45;
             textBlockTitle.Foreground = foreground;
             textBlockTitle.Margin = new Thickness(5, 3, 5, 3);
             textBlockTitle.Text = card.Title;
@@ -200,13 +288,17 @@ namespace TabulaDevApp.MVVM.ViewModels
 
             textDescription.FontSize = 12;
             textDescription.TextWrapping = TextWrapping.Wrap;
-            textDescription.MaxHeight = 60;
+            textDescription.MaxHeight = 40;
             textDescription.Foreground = Brushes.Gray;
             textDescription.Margin = new Thickness(5, 5, 5, 3);
             textDescription.Text = card.Description;
             textDescription.HorizontalAlignment = HorizontalAlignment.Left;
             textDescription.TextTrimming = TextTrimming.WordEllipsis;
 
+            titleGrid.Children.Add(titleCard);
+            titleGrid.Children.Add(rectUnder);
+
+            newPanel.Children.Add(titleGrid);
             newPanel.Children.Add(textBlockTitle);
             newPanel.Children.Add(textDescription);
             newCard.Child = newPanel;
@@ -214,6 +306,24 @@ namespace TabulaDevApp.MVVM.ViewModels
             return newCard;
         }
 
+        // Creat Empty Card
+        private Border CreateUIEmptyCard()
+        {
+            SolidColorBrush foreground = new SolidColorBrush { Color = Color.FromRgb(244, 245, 245) };
+            SolidColorBrush background = new SolidColorBrush { Color = Color.FromRgb(47, 48, 55) };
+
+            Border newCard = new Border();
+
+            newCard.Background = background;
+            newCard.CornerRadius = new CornerRadius(5);
+            newCard.BorderBrush = foreground;
+            newCard.Margin = new Thickness(0, 0, 0, 5);
+            newCard.Width = 215;
+            newCard.Height = 105;
+            return newCard;
+        }
+        
+        // Update display
         private void UpdateStackPanel()
         {
             Button newButton = CreateNewListButton();
@@ -229,6 +339,7 @@ namespace TabulaDevApp.MVVM.ViewModels
             CurrentStackPanel = newStackPanel;
         }
 
+        // Click on card, navigate to Card View
         private void OnClickCard(object sender, MouseButtonEventArgs e)
         {
             Border card = (Border)sender;
@@ -240,6 +351,7 @@ namespace TabulaDevApp.MVVM.ViewModels
             _navigationStore.UpperViewModel = new CardViewModel(_navigationStore, kanbanBoardModel, columnIndex, cardIndex);
         }
 
+        // Keeps track of the display of column name changes
         private void OnColumnTextChanged(object sender, TextChangedEventArgs e)
         {
             TextBox titleColumn = (TextBox)sender;
@@ -247,6 +359,136 @@ namespace TabulaDevApp.MVVM.ViewModels
             int columnIndex = Convert.ToInt32(arrayWordsButton[1]);
 
             kanbanBoardModel.Lists[columnIndex].Title = titleColumn.Text;
+        }
+
+        /// ------------------------
+        /// Functions Drag & Drop
+        /// ------------------------
+
+        // Event - Card mouse move
+        private void Card_MouseMove(object sender, MouseEventArgs e)
+        {
+            Border card = (Border)sender;
+            string[] arrayWordsButton = card.Name.Split(new char[] { '_' });
+            int cardIndex = Convert.ToInt32(arrayWordsButton[1]);
+            int columnIndex = Convert.ToInt32(arrayWordsButton[2]);
+
+            _dragAndDropCurrentCard = kanbanBoardModel.Lists[columnIndex].Cards[cardIndex];
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                //card.IsHitTestVisible = false;
+                DragDrop.DoDragDrop(card, new DataObject(DataFormats.Serializable, card), DragDropEffects.Move);
+                //card.IsHitTestVisible = true;
+            }
+        }
+
+        // Event - Card drop at column
+        private void Column_Drop(object sender, DragEventArgs e)
+        {
+            Canvas column = (Canvas)sender;
+            string[] arrayWordsButton = column.Name.Split(new char[] { '_' });
+            int columnIndex = Convert.ToInt32(arrayWordsButton[1]);
+
+            object data = e.Data.GetData(DataFormats.Serializable);
+
+            if (data is UIElement element)
+            {
+                // Add new Card
+                Point dropPosition = e.GetPosition(column);
+                int cardIndex = Find_CardIndex(columnIndex, dropPosition.Y);
+
+                // Remove old Card
+                string[] arrayWordsOldCard = _dragAndDropCurrentCard.id.Split(new char[] { '_' });
+                int cardIndexOldCard = Convert.ToInt32(arrayWordsOldCard[0]);
+                int columnIndexOldCard = Convert.ToInt32(arrayWordsOldCard[1]);
+
+                kanbanBoardModel.Lists[columnIndexOldCard].Cards.RemoveAt(cardIndexOldCard);
+
+                if (cardIndex >= kanbanBoardModel.Lists[columnIndex].Cards.Count)
+                {
+                    kanbanBoardModel.Lists[columnIndex].Cards.Add(_dragAndDropCurrentCard);
+                }
+                else
+                {
+                    kanbanBoardModel.Lists[columnIndex].Cards.Insert(cardIndex, _dragAndDropCurrentCard);
+                }
+            
+                _dragAndDropCurrentCard = null;
+                UpdateStackPanel();
+            }
+        }
+
+        // Event - Card drag and move
+        private void Column_DragOver(object sender, DragEventArgs e)
+        {
+            Canvas column = (Canvas)sender;
+            object data = e.Data.GetData(DataFormats.Serializable);
+
+            Console.WriteLine(column.Name);
+            if (data is UIElement element)
+            { 
+                Point dropPosition = e.GetPosition(column);
+                Canvas.SetLeft(element, dropPosition.X);
+                Canvas.SetTop(element, dropPosition.Y);
+
+                if (!column.Children.Contains(element))
+                {
+                    Column_DragLeave(sender, e);
+                    column.Children.Add(element);
+                }
+            }
+        }
+
+        private void Column_DragLeave(object sender, DragEventArgs e)
+        {
+            Canvas column = (Canvas)sender;
+            object data = e.Data.GetData(DataFormats.Serializable);
+
+            if (data is UIElement element)
+            {
+                try
+                {
+                    string[] arrayWordsButton = (element as Border).Name.Split(new char[] { '_' });
+                    int cardIndex = Convert.ToInt32(arrayWordsButton[1]);
+                    int columnIndex = Convert.ToInt32(arrayWordsButton[2]);
+                    column.Children.Remove(element);
+                } catch (Exception) { }
+            }
+        }
+
+        private int Find_CardIndex(int columnIndex, double YPosition)
+        {
+            int cardIndex = 0;
+            double countHeight = 50;
+            for(int i = 0; i < kanbanBoardModel.Lists[columnIndex].Cards.Count + 1; i++)
+            {
+                if(YPosition >= countHeight && YPosition <= (countHeight + 115)) {
+                    cardIndex = i;
+                    break;
+                }
+                countHeight += 115;
+            }
+
+            return cardIndex;
+        }
+
+        private void Column_DragEnter(object sender, DragEventArgs e)
+        {
+            Canvas column = (Canvas)sender;
+            string[] arrayWordsButton = column.Name.Split(new char[] { '_' });
+            int columnIndex = Convert.ToInt32(arrayWordsButton[1]);
+
+            object data = e.Data.GetData(DataFormats.Serializable);
+
+            if (data is UIElement element)
+            {
+                // Preview enter
+                Point dropPosition = e.GetPosition(column);
+                _dragAndDropStoreCardIndex = Find_CardIndex(columnIndex, dropPosition.Y);
+                _dragAndDropStoreColumnIndex = columnIndex;
+
+                UpdateStackPanel();
+            }
         }
     }
 }
